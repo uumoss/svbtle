de02b48bc7c107921f7519e221ceda48eb270272
Wesley <uumoss@gmail.com>
2014-03-06 19:36:58 -0700

    html css js changes

--- MLSapp/app/scripts/app.js
+++ MLSapp/app/scripts/app.js
@@ -1,3 +1,10 @@
 'use strict';
 
 angular.module('mlsappApp', []);
+
+var cards = $('.playerCard');
+for(var i = 0; i < cards.length; i++){
+    var target = Math.floor(Math.random() * cards.length -1) + 1;
+    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+    cards.eq(target).before(cards.eq(target2));
+};
