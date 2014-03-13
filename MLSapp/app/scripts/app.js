41bcba1e534fe6b93266401cce77f1d4ce552b25
Wesley <uumoss@gmail.com>
2014-03-13 13:06:18 -0700

    front end changes

--- MLSapp/app/scripts/app.js
+++ MLSapp/app/scripts/app.js
@@ -1,10 +1,24 @@
 'use strict';
 
-angular.module('mlsappApp', []);
+angular.module('mlsappApp', ['angular-underscore']);
 
-var cards = $('.playerCard');
-for(var i = 0; i < cards.length; i++){
-    var target = Math.floor(Math.random() * cards.length -1) + 1;
-    var target2 = Math.floor(Math.random() * cards.length -1) +1;
-    cards.eq(target).before(cards.eq(target2));
-};
+$.ajax({
+  url: 'http://api.randomuser.me/?results=10',
+  dataType: 'json',
+  success: function(data) {
+    // uncomment console.log to view the user Object
+    // console.log(data.results);
+
+    $('.random-user--wrap > img').each(function(i) {
+      $(this).attr('src', data.results[i].user.picture);
+    });
+    
+    $('.container > .random-user--wrap span.fname').each(function(i) {
+      $(this).append(data.results[i].user.name.first);
+    });
+
+    $('.container > .random-user--wrap span.lname').each(function(i) {
+      $(this).append(data.results[i].user.name.last);
+    });
+  }
+});
